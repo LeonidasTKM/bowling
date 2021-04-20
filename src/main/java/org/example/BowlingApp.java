09154f65c54a61c477ccc67bd72b2b3ae6da35d1
TADIE <denis-laurel.tadie@capgemini.com>
2021-04-20 15:17:26 +0200

    add readme

--- src/main/java/org/example/BowlingApp.java
+++ src/main/java/org/example/BowlingApp.java
@@ -10,11 +10,13 @@ import java.text.ParseException;
  */
 public class BowlingApp
 {
-    public static void main( String[] args ) throws ParseException {
+    public static void main( String[] args ) throws Exception {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("No Frame were provided");
+        }
         String frameString = args[0];
         BowlingService bowlingService = new BowlingService();
-
-        Integer score = bowlingService.playBowling(frameString);
+        int score = bowlingService.playBowling(frameString);
         System.out.printf( "Frames : " + frameString + "%n Score : " + score );
     }
 
